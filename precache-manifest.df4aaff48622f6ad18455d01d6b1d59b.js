a2e4056aa99911b8ed8bde4db9b58c6e68c4de91
yaser1rifai <yaser1rifai@gmail.com>
2020-01-06 03:03:06 +0100

    Updates

---
+++ precache-manifest.df4aaff48622f6ad18455d01d6b1d59b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "820303be2f7d0c2f71456cf0aeae9570",
+    "url": "/yaser1rifai/juniortech/index.html"
+  },
+  {
+    "revision": "b4b4cb0a488e48dc7324",
+    "url": "/yaser1rifai/juniortech/static/css/main.34fe90eb.chunk.css"
+  },
+  {
+    "revision": "d08892cfbbca8d1ae6bb",
+    "url": "/yaser1rifai/juniortech/static/js/2.58d16d16.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/yaser1rifai/juniortech/static/js/2.58d16d16.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b4b4cb0a488e48dc7324",
+    "url": "/yaser1rifai/juniortech/static/js/main.6db9f383.chunk.js"
+  },
+  {
+    "revision": "3a2ca4dc3c971a972474",
+    "url": "/yaser1rifai/juniortech/static/js/runtime-main.ec155628.js"
+  },
+  {
+    "revision": "999eb6de7663b3853cbaaaad79999fcd",
+    "url": "/yaser1rifai/juniortech/static/media/job.999eb6de.svg"
+  }
+]);
